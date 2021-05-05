589fdbb4bd1502fa49eaedb5f8ccd23ac88c1ee6
Ram <rameshkqa@gmail.com>
2021-05-04 21:48:12 -0400

    First commit on May 4th

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Ramesh\eclipse-workspace\FirstTest
+mvn clean install
---
+++ src/test/java/com/ram/testcases/TesCase01.java
@@ -0,0 +1,39 @@
+package com.ram.testcases;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TesCase01 {
+	public String baseUrl = "https://www.google.ca/";
+	WebDriver driver;
+
+	@BeforeTest
+	public void launchBrowser() {
+		System.out.println("launching firefox browser");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+	}
+
+	@Test
+	public void verifyTitle() {
+
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Google";
+		System.out.println("Actual Title :" + actualTitle);
+		Assert.assertEquals(actualTitle, expectedTitle);
+		System.out.println("Test Completed");
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.close(); 
+		System.out.println("Google Page Closed");
+	} 
+
+}
